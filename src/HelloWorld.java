f2bc46665fc5a5f9b5ac51d70bac0ad0a7d39728
nahye <knh8783@gmail.com>
2025-04-23 10:52:20 +0900

    init java project

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * javac HelloWolrd.java
+ * java HelloWolrd
+ * Run 실행 단축키 = cntl + f11
+ * 	     우클릭 -> Run Ass
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello Java !!");
+	}
+}
