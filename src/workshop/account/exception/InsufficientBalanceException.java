40d924eab5133754f1f46432620128c32e7b2f9e
nahye <knh8783@gmail.com>
2025-04-23 14:19:03 +0900

    add insufficientbalanceexception

--- src/workshop/account/exception/InsufficientBalanceException.java
+++ src/workshop/account/exception/InsufficientBalanceException.java
@@ -1,5 +1,9 @@
 package workshop.account.exception;
 
-public class InsufficientBalanceException {
-
+public class InsufficientBalanceException extends Exception {
+	
+	public InsufficientBalanceException(String errMessage) {
+		//받은 에러 메세지를 부모한테 넘겨줌
+		super(errMessage);
+	}
 }
